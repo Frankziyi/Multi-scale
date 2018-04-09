977205675121b80706377085f6e5ef374229e33c
Frankziyi <617328440@qq.com>
2018-04-09 09:48:40 +0800

    little bug

--- nets/my_model_single.py
+++ nets/my_model_single.py
@@ -118,6 +118,7 @@ class SubIncption(BaseModel):
     def init_network(self):
         x = self.image
         x = tf.image.resize_images(x, [self.size,self.size], 0) #0 mean bilinear
+        pdb.set_trace()
         x = tf.subtract(x, 0.5)
         x = tf.multiply(x, 2.0)
 
