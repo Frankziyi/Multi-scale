3c3ca0173dbc144a8e7b1b2b050c041c2308148b
Frankziyi <617328440@qq.com>
2018-04-10 21:39:47 +0800

    test duke with inception 299

--- scripts/get_features_single.sh
+++ scripts/get_features_single.sh
@@ -2,7 +2,7 @@
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=/world/data-gpu-94/sysu-reid/checkpoints
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/home/yuanziyi/checkpoints/Duke_inception_225
+TRAIN_DIR=/home/yuanziyi/checkpoints/Duke_inception_299
 # Where the dataset is saved to.
 DATASET_DIR=/world/data-gpu-94/sysu-reid/person-reid-data/DukeMTMC-reID-tfrecord
 # WHere the log is saved to
@@ -23,8 +23,8 @@ python get_features_single.py \
 --log_dir=${LOG_DIR} \
 --optimizer=sgd \
 --weight_decay=0.00004 \
---ckpt_num=49829 \
---scale_size=225
+--ckpt_num=49618 \
+--scale_size=299
 
 python get_features_single.py \
 --dataset_name=Market_1501 \
@@ -38,8 +38,8 @@ python get_features_single.py \
 --log_dir=${LOG_DIR} \
 --optimizer=sgd \
 --weight_decay=0.00004 \
---ckpt_num=52799 \
---scale_size=225
+--ckpt_num=51898 \
+--scale_size=299
 
 python get_features_single.py \
 --dataset_name=Market_1501 \
@@ -53,5 +53,35 @@ python get_features_single.py \
 --log_dir=${LOG_DIR} \
 --optimizer=sgd \
 --weight_decay=0.00004 \
---ckpt_num=55766 \
---scale_size=225
+--ckpt_num=54169 \
+--scale_size=299
+
+python get_features_single.py \
+--dataset_name=Market_1501 \
+--probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+--gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+--model_name=inception_v3 \
+--batch_size=8 \
+--max_number_of_steps=10001 \
+--checkpoint_dir=${TRAIN_DIR} \
+--pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
+--log_dir=${LOG_DIR} \
+--optimizer=sgd \
+--weight_decay=0.00004 \
+--ckpt_num=56150 \
+--scale_size=299
+
+python get_features_single.py \
+--dataset_name=Market_1501 \
+--probe_dataset_dir=${PROBE_OUTPUT_DIR} \
+--gallery_dataset_dir=${GALLERY_OUTPUT_DIR} \
+--model_name=inception_v3 \
+--batch_size=8 \
+--max_number_of_steps=10001 \
+--checkpoint_dir=${TRAIN_DIR} \
+--pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
+--log_dir=${LOG_DIR} \
+--optimizer=sgd \
+--weight_decay=0.00004 \
+--ckpt_num=58228 \
+--scale_size=299
