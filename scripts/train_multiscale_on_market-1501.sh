40be4f09fc3d41a6f369327cc1794c5958dde587
Frankziyi <617328440@qq.com>
2018-04-12 23:47:43 +0800

    correct the train

--- scripts/train_multiscale_on_market-1501.sh
+++ scripts/train_multiscale_on_market-1501.sh
@@ -8,25 +8,25 @@ LOG_DIR=/world/data-gpu-94/sysu-reid/zhangkaicheng/log
 # Wher the tfrecord file is save to
 DATASET_DIR=/world/data-gpu-94/sysu-reid/person-reid-data/DukeMTMC-reID-tfrecord/bounding_box_train
 
-python train_multiscale.py \
---learning_rate=2e-4 \
---learning_rate_decay_type=fixed \
---dataset_dir=${DATASET_DIR} \
---model_name=inception_v3 \
---batch_size=8 \
---max_number_of_steps=20000 \
---checkpoint_dir=${CKPT_SAVE_DIR} \
---log_dir=${LOG_DIR} \
---save_model_summary_secs=360 \
---log_every_n_steps=100 \
---optimizer=adam \
---adam_beta1=0.5 \
---adam_beta2=0.999 \
---weight_decay=0.00004 \
---GPU_use=6 \
---pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
---pretrain_branch_0_path=${PRETRAINED_CHECKPOINT_DIR}/Duke_inception_299/model.ckpt-49618 \
---pretrain_branch_1_path=${PRETRAINED_CHECKPOINT_DIR}/Duke_inception_225/model.ckpt-58714
+# python train_multiscale.py \
+# --learning_rate=2e-4 \
+# --learning_rate_decay_type=fixed \
+# --dataset_dir=${DATASET_DIR} \
+# --model_name=inception_v3 \
+# --batch_size=8 \
+# --max_number_of_steps=20000 \
+# --checkpoint_dir=${CKPT_SAVE_DIR} \
+# --log_dir=${LOG_DIR} \
+# --save_model_summary_secs=360 \
+# --log_every_n_steps=100 \
+# --optimizer=adam \
+# --adam_beta1=0.5 \
+# --adam_beta2=0.999 \
+# --weight_decay=0.00004 \
+# --GPU_use=6 \
+# --pretrain_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
+# --pretrain_branch_0_path=${PRETRAINED_CHECKPOINT_DIR}/Duke_inception_299/model.ckpt-49618 \
+# --pretrain_branch_1_path=${PRETRAINED_CHECKPOINT_DIR}/Duke_inception_225/model.ckpt-58714
 
 python train_multiscale.py \
 --learning_rate=5e-5 \
